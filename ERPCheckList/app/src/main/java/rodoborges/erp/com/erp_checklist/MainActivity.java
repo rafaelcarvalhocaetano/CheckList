e61dbfc64c304e08c234d9712c610f4efda33eba
Rafael <rapha.pse@outlook.com>
2017-11-14 21:49:24 -0200

    Add App RB CheckList

--- ERPCheckList/app/src/main/java/rodoborges/erp/com/erp_checklist/MainActivity.java
+++ ERPCheckList/app/src/main/java/rodoborges/erp/com/erp_checklist/MainActivity.java
@@ -2,12 +2,25 @@ package rodoborges.erp.com.erp_checklist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+   // private DatabaseReference db = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // db.child("pontos").setValue("100");
+
+
+
+
     }
 }
