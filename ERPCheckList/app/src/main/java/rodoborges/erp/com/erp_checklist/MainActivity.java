07c1819a620e8aba8587f7bd7c1dde4fc2ba01c4
Rafael <rapha.pse@outlook.com>
2017-11-14 17:30:20 -0200

    Projeto Check-List RB para o setor da manutenção

---
+++ ERPCheckList/app/src/main/java/rodoborges/erp/com/erp_checklist/MainActivity.java
@@ -0,0 +1,13 @@
+package rodoborges.erp.com.erp_checklist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
